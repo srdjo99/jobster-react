7117e706e916228d972423aa21e2f789523d61f9
Srdjan <srdjancoralic5@gmail.com>
2022-09-29 20:36:56 +0200

    Landing page structure

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,12 @@
 import { FC, ReactElement } from "react";
+import styled from "styled-components";
 
 import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
 
 const Landing: FC = (): ReactElement => {
   return (
-    <main>
+    <Wrapper>
       <nav>
         <img src={logo} alt="jobster logo" className="logo" />
       </nav>
@@ -21,10 +23,55 @@ const Landing: FC = (): ReactElement => {
           </p>
           <button className="btn btn-hero">Login/Register</button>
         </div>
-        <div />
+        <img src={main} alt="successful deal" className="img main-img" />
       </div>
-    </main>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+
+  h1 {
+    font-weight: 700;
+
+    span {
+      color: var(--primary-500);
+    }
+  }
+
+  p {
+    color: var(--grey-600);
+  }
+
+  .main-img {
+    display: none;
+  }
+
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+
+    .main-img {
+      display: block;
+    }
+  }
+`;
 
 export default Landing;
