ad42fd3c5c386ab78dbb5c2bfbe370b20b017d62
Srdjan <srdjancoralic5@gmail.com>
2022-09-29 14:28:46 +0200

    Add landing page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { ReactElement } from "react";
+
 import Landing from "./pages/Landing";
 
-function App() {
+function App(): ReactElement {
   return (
     <div>
       <Landing />
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import { FC, ReactElement } from "react";
 
 import logo from "../assets/images/logo.svg";
-import main from "../assets/images/main.svg";
 
-interface Props {}
-
-const Landing = (props: Props) => {
-  return <h1>Landing page</h1>;
+const Landing: FC = (): ReactElement => {
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+            numquam soluta odit, quia possimus quas excepturi pariatur
+            doloremque saepe voluptates.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <div />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
