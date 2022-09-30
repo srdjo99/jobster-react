81b75fdc41e892674b19be60bd8807a701348134
Srdjan <srdjancoralic5@gmail.com>
2022-09-30 10:02:52 +0200

    Add toggle member functionality

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,73 @@
-import { FC, ReactElement } from "react";
+import React, { FC, useState, useEffect, ReactElement } from "react";
+
+import { Logo, FormRow } from "../components";
+
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const Register: FC = (): ReactElement => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e: React.SyntheticEvent): void => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  const toggleMember = (): void => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {/* name field */}
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+
+        {/* email field */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password field */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          {values.isMember ? "Not a member yet? " : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
