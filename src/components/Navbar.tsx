e1444a5e2872d5f1db9f0a6d23b814043c4c9def
Srdjan <srdjancoralic5@gmail.com>
2022-10-03 11:36:32 +0200

    Navbar setup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,52 @@
-import React, { ReactElement } from "react";
-import { FaHome } from "react-icons/fa";
+import React, { useState, ReactElement } from "react";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
 
+import { useDispatch, useSelector } from "react-redux";
+
+import { RootState, AppDispatch } from "../store";
 import Wrapper from "../assets/wrappers/Navbar";
+import Logo from "./Logo";
 
 const Navbar = (): ReactElement => {
+  const { user } = useSelector((store: RootState) => store.user);
+
+  const dispatch = useDispatch<AppDispatch>();
+
   return (
     <Wrapper>
-      <h2>Navbar</h2>
-      <FaHome />
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={() => console.log("toggle sidebar")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log("toggle logout dropdown")}
+          >
+            <FaUserCircle />
+            {user?.name}
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              className="dropdown-btn"
+              type="button"
+              onClick={() => console.log("logout user")}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
