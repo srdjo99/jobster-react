2680ed6d4f10a6b7e8ed6f84ed86d1df592c1333
Srdjan <srdjancoralic5@gmail.com>
2022-10-20 09:29:04 +0200

    AreaChart setup

--- src/components/AreaChart.tsx
+++ src/components/AreaChart.tsx
@@ -1,5 +1,28 @@
-const AreaChart = () => {
-  return <h2>AreaChart</h2>;
+import { FC } from "react";
+import {
+  Area,
+  XAxis,
+  YAxis,
+  Tooltip,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
+
+import { IMonthlyApplications } from "../types/IStats";
+
+const AreaChartComponent: FC<{ data: IMonthlyApplications[] }> = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray=" 3 3" />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Area type="monotone" dataKey="count" stroke="#2cb1bc" fill="#bef8fd" />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
 };
 
-export default AreaChart;
+export default AreaChartComponent;
--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,4 +1,7 @@
-const BarChart = () => {
+import { FC } from "react";
+import { IMonthlyApplications } from "../types/IStats";
+
+const BarChart: FC<{ data: IMonthlyApplications[] }> = ({ data }) => {
   return <h2>BarChart</h2>;
 };
 
